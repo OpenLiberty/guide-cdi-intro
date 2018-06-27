0ab9279165751e82d40b60903313b559f87d777b
Panagiotis Roubatsis <proubatsis@gmail.com>
2018-06-27 11:30:56 -0400

    Change internal handling of systems

--- finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
+++ finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
@@ -13,15 +13,18 @@
 package io.openliberty.guides.inventory.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InventoryList {
 
-  private List<System> systems = new ArrayList<System>();
+  private Map<String, System> systems = new ConcurrentHashMap<>();
 
   public List<System> getSystems() {
-    return systems;
+    return new ArrayList<System>(systems.values());
   }
 
   public int getTotal() {
@@ -32,10 +35,7 @@ public class InventoryList {
     Properties props = new Properties();
     props.setProperty("os.name", systemProps.getProperty("os.name"));
     props.setProperty("user.name", systemProps.getProperty("user.name"));
-
-    System host = new System(hostname, props);
-    if (!systems.contains(host))
-      systems.add(host);
+    systems.putIfAbsent(hostname, new System(hostname, props));
   }
 
   class System {
