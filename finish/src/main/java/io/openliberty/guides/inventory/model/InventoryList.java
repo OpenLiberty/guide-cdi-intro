cd861b2be229b77e7f0a05df50d19b4845bcb9d9
Panagiotis Roubatsis <proubatsis@gmail.com>
2018-06-27 11:31:44 -0400

    Remove unused import

--- finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
+++ finish/src/main/java/io/openliberty/guides/inventory/model/InventoryList.java
@@ -13,7 +13,6 @@
 package io.openliberty.guides.inventory.model;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
