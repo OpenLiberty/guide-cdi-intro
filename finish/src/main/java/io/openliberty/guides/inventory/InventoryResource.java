e5dc15f5b0d4bbeaab39de5c79c6e1426da49692
Justine Chen <justine.chenibm.com@justines-mbp.war.can.ibm.com>
2019-03-20 13:27:25 -0400

    Updated ordering of manager/systemclient

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -1,15 +1,3 @@
-// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2017, 2019 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - Initial implementation
- *******************************************************************************/
-// end::copyright[]
 package io.openliberty.guides.inventory;
 
 import java.util.Properties;
@@ -24,19 +12,15 @@ import javax.ws.rs.core.Response;
 import io.openliberty.guides.inventory.model.InventoryList;
 import io.openliberty.guides.inventory.client.SystemClient;
 
-// tag::RequestScoped[]
 @RequestScoped
-// end::RequestScoped[]
 @Path("/systems")
 public class InventoryResource {
 
   @Inject
-  private SystemClient systemClient;
-
-  // tag::Inject[]
-  @Inject
   InventoryManager manager;
-  // end::Inject[]
+
+  @Inject
+  SystemClient systemClient;
 
   @GET
   @Path("/{hostname}")
