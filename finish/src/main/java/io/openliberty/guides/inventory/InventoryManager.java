7216550f4d5ea7d26bcfdb08d1960d75b0da8781
Mai Hameed <maihameed@hotmail.com>
2019-05-24 16:08:26 -0400

    removed unecessary imports

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -16,11 +16,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
-import io.openliberty.guides.inventory.client.SystemClient;
 import io.openliberty.guides.inventory.model.InventoryList;
 import io.openliberty.guides.inventory.model.SystemData;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 // tag::ApplicationScoped[]
 @ApplicationScoped
