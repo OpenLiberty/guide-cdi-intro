7883310faa55e3170e35074f78b4e57b7a38973d
Nimrah Gill <Nimrah.Gill@ibm.com>
2019-10-21 13:44:26 -0400

    fixed weird spacing

--- finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -33,62 +33,62 @@ public class SystemClient {
 
   // Wrapper function that gets properties
   public Properties getProperties(String hostname) {
-    String url = buildUrl(PROTOCOL, hostname, DEFAULT_PORT, SYSTEM_PROPERTIES);	
-    Builder clientBuilder = buildClientBuilder(url);	
-    return getPropertiesHelper(clientBuilder);	
+    String url = buildUrl(PROTOCOL, hostname, Integer.valueOf(DEFAULT_PORT), SYSTEM_PROPERTIES);
+    Builder clientBuilder = buildClientBuilder(url);
+    return getPropertiesHelper(clientBuilder);
   }
 
-  // tag::doc[]	
-  /**	
-   * Builds the URI string to the system service for a particular host.	
-   * @param protocol	
-   *          - http or https.	
-   * @param host	
-   *          - name of host.	
-   * @param port	
-   *          - port number.	
-   * @param path	
-   *          - Note that the path needs to start with a slash!!!	
-   * @return String representation of the URI to the system properties service.	
-   */	
-  // end::doc[]	
-  protected String buildUrl(String protocol, String host, int port, String path) {	
-    try {	
-      URI uri = new URI(protocol, null, host, port, path, null, null);	
-      return uri.toString();	
-    } catch (Exception e) {	
-      System.err.println("Exception thrown while building the URL: " + e.getMessage());	
-      return null;	
-    }	
-  }	
+  // tag::doc[]
+  /**
+   * Builds the URI string to the system service for a particular host.
+   * @param protocol
+   *          - http or https.
+   * @param host
+   *          - name of host.
+   * @param port
+   *          - port number.
+   * @param path
+   *          - Note that the path needs to start with a slash!!!
+   * @return String representation of the URI to the system properties service.
+   */
+  // end::doc[]
+  protected String buildUrl(String protocol, String host, int port, String path) {
+    try {
+      URI uri = new URI(protocol, null, host, port, path, null, null);
+      return uri.toString();
+    } catch (Exception e) {
+      System.err.println("Exception thrown while building the URL: " + e.getMessage());
+      return null;
+    }
+  }
 
-  // Method that creates the client builder	
-  protected Builder buildClientBuilder(String urlString) {	
-    try {	
-      Client client = ClientBuilder.newClient();	
-      Builder builder = client.target(urlString).request();	
-      return builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);	
-    } catch (Exception e) {	
-      System.err.println("Exception thrown while building the client: " + e.getMessage());	
-      return null;	
-    }	
-  }	
+  // Method that creates the client builder
+  protected Builder buildClientBuilder(String urlString) {
+    try {
+      Client client = ClientBuilder.newClient();
+      Builder builder = client.target(urlString).request();
+      return builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+    } catch (Exception e) {
+      System.err.println("Exception thrown while building the client: " + e.getMessage());
+      return null;
+    }
+  }
 
-  // Helper method that processes the request	
-  protected Properties getPropertiesHelper(Builder builder) {	
-    try {	
-      Response response = builder.get();	
-      if (response.getStatus() == Status.OK.getStatusCode()) {	
-        return response.readEntity(Properties.class);	
-      } else {	
-        System.err.println("Response Status is not OK.");	
-      }	
-    } catch (RuntimeException e) {	
-      System.err.println("Runtime exception: " + e.getMessage());	
-    } catch (Exception e) {	
-      System.err.println("Exception thrown while invoking the request: " + e.getMessage());	
-    }	
-    return null;	
-  }	
+  // Helper method that processes the request
+  protected Properties getPropertiesHelper(Builder builder) {
+    try {
+      Response response = builder.get();
+      if (response.getStatus() == Status.OK.getStatusCode()) {
+        return response.readEntity(Properties.class);
+      } else {
+        System.err.println("Response Status is not OK.");
+      }
+    } catch (RuntimeException e) {
+      System.err.println("Runtime exception: " + e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Exception thrown while invoking the request: " + e.getMessage());
+    }
+    return null;
+  }
 
 }
